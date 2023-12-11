2aeee98e63b7d596b6e5f6a655f61110fb16f68e
zadaaar <zadaalfarras@gmail.com>
2023-12-11 13:34:00 +0700

    fix nilairapor dan auto increment

--- autoincrement.sql
+++ autoincrement.sql
@@ -1,4 +1,4 @@
 delete from namatabel
-DBCC CHECKIDENT ('namatabel', RESEED, 1);
+DBCC CHECKIDENT ('namatabel', RESEED, 0);
 
 --Command ini digunakan kalau auto incrementnya udah jalan gak start dari 1

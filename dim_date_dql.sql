79e50c2e410a5d47c6adde0438099593680c6007
naufalfano <86518546+naufalfano@users.noreply.github.com>
2023-12-13 17:25:53 +0700

    Update dim_date_dql.sql

--- dim_date_dql.sql
+++ dim_date_dql.sql
@@ -1,20 +1,20 @@
-DECLARE @StartDate DATE = '2000-01-01';
-DECLARE @EndDate DATE = '2023-12-31';
+DECLARE @StartDate date = '2000-01-01';
+DECLARE @EndDate date = '2023-12-31';
 WHILE @StartDate <= @EndDate
 BEGIN
-INSERT INTO [DW_AdventureWorks2016].[dbo].[dim_date] (Date, Day,
-Month, Quarter, QuarterName, Year)
+INSERT INTO [olap_admisi].[dbo].[dim_waktu] (date, day,
+month, quarter, quarterName, year)
 VALUES (
 @StartDate,
-DATEPART(DAY, @StartDate),
-DATEPART(MONTH, @StartDate),
-DATEPART(QUARTER, @StartDate),
-CASE WHEN DATEPART(QUARTER, @StartDate) = 1 THEN 'Q1'
-WHEN DATEPART(QUARTER, @StartDate) = 2 THEN 'Q2'
-WHEN DATEPART(QUARTER, @StartDate) = 3 THEN 'Q3'
-WHEN DATEPART(QUARTER, @StartDate) = 4 THEN 'Q4'
+DATEPART(day, @StartDate),
+DATEPART(month, @StartDate),
+DATEPART(quarter, @StartDate),
+CASE WHEN DATEPART(quarter, @StartDate) = 1 THEN 'Q1'
+WHEN DATEPART(quarter, @StartDate) = 2 THEN 'Q2'
+WHEN DATEPART(quarter, @StartDate) = 3 THEN 'Q3'
+WHEN DATEPART(quarter, @StartDate) = 4 THEN 'Q4'
 END,
-DATEPART(YEAR, @StartDate)
+DATEPART(year, @StartDate)
 );
-SET @StartDate = DATEADD(DAY, 1, @StartDate);
+SET @StartDate = DATEADD(day, 1, @StartDate);
 END;

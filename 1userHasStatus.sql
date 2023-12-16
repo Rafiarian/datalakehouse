bce8b3a624c4a981ab0163c9ae5422191b9fdad5
zadaaar <70267782+zadaaar@users.noreply.github.com>
2023-12-16 23:28:16 +0700

    Tambah baris 'Mahasiswa Baru'

--- 1userHasStatus.sql
+++ 1userHasStatus.sql
@@ -16,5 +16,6 @@ INSERT INTO userHasStatus (namaStatus) VALUES ('Tidak Lolos Seleksi Mandiri Umum
 INSERT INTO userHasStatus (namaStatus) VALUES ('Tidak Lolos Seleksi Mandiri Kemitraan');
 INSERT INTO userHasStatus (namaStatus) VALUES ('Tidak Lolos Seleksi IUP');
 INSERT INTO userHasStatus (namaStatus) VALUES ('Tidak Lolos Seleksi Mandiri Prestasi');
+INSERT INTO userHasStatus (namaStatus) VALUES ('Mahasiswa Baru');
 
 select * from userHasStatus
